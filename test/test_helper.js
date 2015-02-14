ee08024aba8238eeb784a0b82dc80bc5a659c97f
Tom Robinson <tlrobinson@gmail.com>
2015-02-13 18:23:59 -0800

    Update test helper to create global IP, InputPort etc

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,23 +2,30 @@ var chai = require('chai');
 
 global.expect = chai.expect;
 global.fbp = require('..');
+global.Fiber = require('fibers');
+
+global.IP = require('../core/IP');
+global.InputPort = require('../core/InputPort');
+global.InputPortArray = require('../core/InputPortArray');
+global.OutputPort = require('../core/OutputPort');
+global.OutputPortArray = require('../core/OutputPortArray');
 
 global.MockSender = function(inputArray) {
   return function() {
-    var outport = fbp.OutputPort.openOutputPort('OUT');
+    var outport = OutputPort.openOutputPort('OUT');
     inputArray.forEach(function(item) {
-      outport.send(fbp.IP.create(item));
+      outport.send(IP.create(item));
     });
   }
 }
 
 global.MockReceiver = function(outputArray) {
   return function() {
-    var inport = fbp.InputPort.openInputPort('IN');
+    var inport = InputPort.openInputPort('IN');
     var ip;
     while ((ip = inport.receive()) !== null) {
       outputArray.push(ip.contents);
-      fbp.IP.drop(ip);
+      IP.drop(ip);
     }
   }
 }
